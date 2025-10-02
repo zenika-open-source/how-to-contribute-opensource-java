7e7b97040bc6ae14f16dfe8eca11df36dde417c8
Souad <62654985+fel31@users.noreply.github.com>
2025-10-02 15:13:48 +0200

    Update Exercise.java model

--- src/main/java/com/example/exercice/model/Exercise.java
+++ src/main/java/com/example/exercice/model/Exercise.java
@@ -1,4 +1,3 @@
-
 package com.example.exercice.model;
 
 import javax.persistence.Entity;
@@ -6,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * Représente un exercice avec un identifiant, un nom et une description.
+ */
 @Entity
 public class Exercise {
 
@@ -15,34 +17,73 @@ public class Exercise {
     private String name;
     private String description;
 
+    /**
+     * Constructeur par défaut.
+     */
     public Exercise() {
     }
 
+    /**
+     * Constructeur pour initialiser un exercice avec un nom et une description.
+     *
+     * @param name        Le nom de l'exercice.
+     * @param description La description de l'exercice.
+     */
     public Exercise(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
+    /**
+     * Obtient l'identifiant de l'exercice.
+     *
+     * @return L'identifiant de l'exercice.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Définit l'identifiant de l'exercice.
+     *
+     * @param id L'identifiant à définir.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Obtient le nom de l'exercice.
+     *
+     * @return Le nom de l'exercice.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Définit le nom de l'exercice.
+     *
+     * @param name Le nom à définir.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Obtient la description de l'exercice.
+     *
+     * @return La description de l'exercice.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Définit la description de l'exercice.
+     *
+     * @param description La description à définir.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
